d6cf64b49a4e8e3984014dae96c8fb1bcf0bdf3c
AadityaBodke <aaditya.bodke0601@gmail.com>
2025-01-06 06:50:58 -0600

    updated evaluation code and README

--- classification/custom_evaluation_mesh.sh
+++ classification/custom_evaluation_mesh.sh
@@ -1,28 +1,27 @@
-embeddings_folder="/scratch/lamdo/scirepeval_classification/embeddings/"
-dataset_name="scirepeval_mesh_test"
+embeddings_folder="/home/abodke2/kpg/ERU-KG/classification/scirepeval_classification/embeddings"
+dataset_name="scirepeval_mesh_descriptors_test"
 
 
 # no expansion
-EMBEDDING_FILE="${embeddings_folder}/embeddings--specter2_base--scirepeval_mesh_test.json" \
+EMBEDDING_FILE="${embeddings_folder}/embeddings--specter2_base--scirepeval_mesh_descriptors_test.json" \
 EXPERIMENT_NAME="no_expansion" \
 python custom_evaluation_mesh.py
 
-for i in $(seq 1 3);
-do
-    EMBEDDING_FILE="${embeddings_folder}/embeddings--specter2_base--scirepeval_mesh_test_keyphrase_expansion_retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-${i}_position_penalty+length_penalty.json" \
-    EXPERIMENT_NAME="eru-kg-${i}" \
-    python custom_evaluation_mesh.py
+# for i in $(seq 1 3);
+# do
+#     EMBEDDING_FILE="${embeddings_folder}/embeddings--specter2_base--scirepeval_mesh_descriptors_test_keyphrase_expansion_retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-${i}_position_penalty+length_penalty.json" \
+#     EXPERIMENT_NAME="eru-kg-${i}" \
+#     python custom_evaluation_mesh.py
 
-    EMBEDDING_FILE="${embeddings_folder}/embeddings--specter2_base--scirepeval_mesh_test_keyphrase_expansion_copyrnn-${i}.json" \
-    EXPERIMENT_NAME="copyrnn-${i}" \
-    python custom_evaluation_mesh.py
+#     EMBEDDING_FILE="${embeddings_folder}/embeddings--specter2_base--scirepeval_mesh_descriptors_test_keyphrase_expansion_copyrnn-${i}.json" \
+#     EXPERIMENT_NAME="copyrnn-${i}" \
+#     python custom_evaluation_mesh.py
 
-    EMBEDDING_FILE="${embeddings_folder}/embeddings--specter2_base--scirepeval_mesh_test_keyphrase_expansion_autokeygen-${i}.json" \
-    EXPERIMENT_NAME="autokeygen-${i}" \
-    python custom_evaluation_mesh.py
+#     EMBEDDING_FILE="${embeddings_folder}/embeddings--specter2_base--scirepeval_mesh_descriptors_test_keyphrase_expansion_autokeygen-${i}.json" \
+#     EXPERIMENT_NAME="autokeygen-${i}" \
+#     python custom_evaluation_mesh.py
 
-    EMBEDDING_FILE="${embeddings_folder}/embeddings--specter2_base--scirepeval_mesh_test_keyphrase_expansion_uokg-${i}.json" \
-    EXPERIMENT_NAME="uokg-${i}" \
-    python custom_evaluation_fos.py
-done
-
+#     EMBEDDING_FILE="${embeddings_folder}/embeddings--specter2_base--scirepeval_mesh_descriptors_test_keyphrase_expansion_uokg-${i}.json" \
+#     EXPERIMENT_NAME="uokg-${i}" \
+#     python custom_evaluation_mesh.py
+# done
--- classification/custom_generate_embeddings.sh
+++ classification/custom_generate_embeddings.sh
@@ -1,50 +1,47 @@
-embeddings_folder="/home/abodke2/kpg/ERU-KG/classification/scirepeval_classification/embeddings"
-classification_folder="/home/abodke2/kpg/ERU-KG/classification/scirepeval_classification"
+# embeddings_folder="/scratch/lamdo/scirepeval_classification/embeddings/"
+embeddings_folder="/home/abodke2/kpg/ERU-KG/classification/arxiv_classification/embeddings_only_present"
+
+classification_folder="/home/abodke2/kpg/ERU-KG/classification/arxiv_classification"
+
+folder_name='arxiv_classification_only_present'
+# folder_name='arxiv_classification'
+# folder_name='scirepeval_fos_test'
+# folder_name='scirepeval_mesh_descriptors_test'
+
 # dataset_name="scirepeval_fos_test"
-dataset_name="scirepeval_mesh_descriptors_test"
+# dataset_name="scirepeval_mesh_descriptors_test"
+dataset_name="arxiv_classification"
+
 # also change in .py file!!!
 
-# CUDA_VISIBLE_DEVICES=0 \
-# EMBEDDINGS_SAVE_PATH="${embeddings_folder}/embeddings--specter2_base--${dataset_name}.json" \
-# DATASET_PATH="${classification_folder}/${dataset_name}/${dataset_name}.json" \
-# KEYPHRASE_EXPANSION=0 \
-# python custom_generate_embeddings.py
-
-
 CUDA_VISIBLE_DEVICES=0 \
-EMBEDDINGS_SAVE_PATH="${embeddings_folder}/embeddings--specter2_base--${dataset_name}_keyphrase_expansion_copyrnn-1.json" \
-DATASET_PATH="${classification_folder}/${dataset_name}/${dataset_name}_keyphrase_expansion_copyrnn-1.json" \
+EMBEDDINGS_SAVE_PATH="${embeddings_folder}/embeddings--specter2_base--${dataset_name}.json" \
+DATASET_PATH="${classification_folder}/${dataset_name}/${dataset_name}.json" \
+KEYPHRASE_EXPANSION=0 \
 python custom_generate_embeddings.py
 
-
-CUDA_VISIBLE_DEVICES=0 \
-EMBEDDINGS_SAVE_PATH="${embeddings_folder}/embeddings--specter2_base--${dataset_name}_keyphrase_expansion_autokeygen-1.json" \
-DATASET_PATH="${classification_folder}/${dataset_name}/${dataset_name}_keyphrase_expansion_autokeygen-1.json" \
-python custom_generate_embeddings.py
-
-
-for i in $(seq 2 3);
+for i in $(seq 1 3);
 do
     CUDA_VISIBLE_DEVICES=0 \
     EMBEDDINGS_SAVE_PATH="${embeddings_folder}/embeddings--specter2_base--${dataset_name}_keyphrase_expansion_uokg-${i}.json" \
-    DATASET_PATH="${classification_folder}/${dataset_name}/${dataset_name}_keyphrase_expansion_uokg-${i}.json" \
+    DATASET_PATH="${classification_folder}/${folder_name}/${dataset_name}_keyphrase_expansion_uokg-${i}.json" \
     python custom_generate_embeddings.py
 
 
     CUDA_VISIBLE_DEVICES=0 \
     EMBEDDINGS_SAVE_PATH="${embeddings_folder}/embeddings--specter2_base--${dataset_name}_keyphrase_expansion_retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-${i}_position_penalty+length_penalty.json" \
-    DATASET_PATH="${classification_folder}/${dataset_name}/${dataset_name}_keyphrase_expansion_retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-${i}_position_penalty+length_penalty.json" \
+    DATASET_PATH="${classification_folder}/${folder_name}/${dataset_name}_keyphrase_expansion_retrieval_based_ukg_custom_trained_combined_references_nounphrase_v6-${i}_position_penalty+length_penalty.json" \
     python custom_generate_embeddings.py
 
 
     CUDA_VISIBLE_DEVICES=0 \
     EMBEDDINGS_SAVE_PATH="${embeddings_folder}/embeddings--specter2_base--${dataset_name}_keyphrase_expansion_copyrnn-${i}.json" \
-    DATASET_PATH="${classification_folder}/${dataset_name}/${dataset_name}_keyphrase_expansion_copyrnn-${i}.json" \
+    DATASET_PATH="${classification_folder}/${folder_name}/${dataset_name}_keyphrase_expansion_copyrnn-${i}.json" \
     python custom_generate_embeddings.py
 
 
     CUDA_VISIBLE_DEVICES=0 \
     EMBEDDINGS_SAVE_PATH="${embeddings_folder}/embeddings--specter2_base--${dataset_name}_keyphrase_expansion_autokeygen-${i}.json" \
-    DATASET_PATH="${classification_folder}/${dataset_name}/${dataset_name}_keyphrase_expansion_autokeygen-${i}.json" \
+    DATASET_PATH="${classification_folder}/${folder_name}/${dataset_name}_keyphrase_expansion_autokeygen-${i}.json" \
     python custom_generate_embeddings.py
 done

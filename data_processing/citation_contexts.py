b23b6aedf1d1b9190cbf56c42220e26406b255cd
Lam Do <louisdo15111999@gmail.com>
2025-06-04 12:31:49 -0500

    Added readme for data_processing

--- data_processing/citation_contexts.py
+++ data_processing/citation_contexts.py
@@ -120,7 +120,9 @@ def main():
     CONFIGS["max_citations_per_sentence"] = max_citations_per_sentence
     CONFIGS["min_section_length"] = min_section_length
 
-    folders = os.listdir(input_folder)
+    folders = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", 
+           "10", "11", "12", "13", "14", "15", "16", "17", "18", 
+           "19", "20", "21", "22", "93", "97", "98"]
     folders = [os.path.join(input_folder, folder) for folder in folders]
 
     with open(arxive_metadata_file) as f:
